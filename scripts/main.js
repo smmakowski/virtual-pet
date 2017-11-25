4df8c5a7c0e7c81eaf86faa6e80fd9b00506fafd
smmakowski <makowski.sm@gmail.com>
2017-11-25 11:35:41 -0800

    implmement pet death and auto decrease stats

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,20 @@
-// game state object
-var GameState = {
+// styles for text
+const headerStyle = {
+  font: '30px  Impact',
+  fill: 'black',
+};
+
+const normalTextStyle = {
+  font: '25px  Impact',
+  fill: 'Gray',
+};
+const lowTextStyle = {
+  font: '25px  Impact',
+  fill: 'Red',
+};
+
+//game state object
+let GameState = {
   //
   isRotating: false,
   rotationDirection: -5,
@@ -34,12 +49,9 @@ var GameState = {
     this.pet.anchor.setTo(.5);
     this.pet.customParams = {health: 100, fun: 100};
     this.pet.inputEnabled = true;
+    this.pet.input.pixelPerfectClick = true;
     this.pet.input.enableDrag(this.pickItem, this);
-    this.pet.events.onInputDown.add(function() {
-      self.rotate.alpha = 1;
-      self.isRotating = false;
-      self.pet.angle = 0;
-    });
+    this.pet.events.onInputDown.add(this.stopRotation,this);
 
     // create buttons
     this.apple = this.game.add.sprite(this.game.world.width * .125, 570, 'apple');
@@ -68,6 +80,7 @@ var GameState = {
     this.rotate.anchor.setTo(.5);
     this.rotate.inputEnabled = true;
     this.rotate.events.onInputDown.add(this.rotatePet, this);
+    // add handler using anon function
     this.rotate.events.onInputUp.add(function() {
       if (self.rotationDirection === 5) {
         self.rotate.scale.setTo(-1, 1);
@@ -77,13 +90,38 @@ var GameState = {
 
     }, this);
 
-    this.buttons = [this.apple, this.candy, this.rubberDuck, this.rotate];
+    this.items = [this.apple, this.candy, this.rubberDuck];
+    // styles for
 
-    // add text
+    // add header text
+    this.game.add.text(this.game.width * .1, this.game.height * .1, 'Health:', headerStyle);
+    this.game.add.text(this.game.width * .6, this.game.height * .1, 'Fun:', headerStyle);
+
+    this.healthText = this.game.add.text(this.game.width * .4, this.game.height * .1, '', normalTextStyle);
+    this.funText = this.game.add.text(this.game.width * .8, this.game.height * .1, '', normalTextStyle);
+
+    this.updateStats();
+
+    this.agePet = this.game.time.events.loop(Phaser.Timer.SECOND * 1, this.decreaseStats, this);
+
   },
   update: function() { // update function
+    const self = this;
     if (this.isRotating) {
       this.pet.angle += this.rotationDirection;
+    }
+    if (this.pet.customParams.health <= 0  || this.pet.customParams.fun <= 0) {
+      this.uiBlocked = true;
+      this.time.events.remove(self.agePet);
+      this.stopRotation();
+
+      this.pet.frame = 4;
+      // let deathAnimation = this.pet.animations.add('die', [1, 2, 3, 2, 1], 7, false);
+      //
+      // deathAnimation.onComplete.add(function() {
+      //   console.log('dead');
+      // }, this);
+      // deathAnimation.play('die');
     }
   },
 
@@ -94,26 +132,11 @@ var GameState = {
       this.clearSelection();
       sprite.alpha = .4;
       this.selectedItem = sprite;
-      if (sprite.customParams.health) {
-        if (this.pet.customParams.health + sprite.customParams.health > 100) {
-          this.pet.customParams.health = 100;
-        } else {
-          this.pet.customParams.health += sprite.customParams.health;
-        }
-      }
-      if (sprite.customParams.fun) {
-        if (this.pet.customParams.fun + sprite.customParams.fun > 100) {
-          this.pet.customParams.fun = 100;
-        } else {
-          this.pet.customParams.fun += sprite.customParams.fun;
-        }
-      }
-      console.log(self.pet.customParams.health);
     }
   },
   rotatePet: function(sprite, event) {
     if (!this.uiBlocked) {
-      this.clearSelection();
+      // this.clearSelection();
       sprite.scale.setTo(.8);
       sprite.alpha = .4;
 
@@ -123,21 +146,26 @@ var GameState = {
   },
 
   stopRotation: function() {
-
+    const self = this;
+    self.rotate.alpha = 1;
+    self.isRotating = false;
+    self.pet.angle = 0;
   },
 
   dropItem: function(sprite, event) {
     const self = this;
-    this.isRotating = false;
     if (this.selectedItem && !this.uiBlocked) {
       this.uiBlocked = true;
+      this.stopRotation();
       const x = event.x;
       const y = event.y;
       let newItem = this.game.add.sprite(x, y, this.selectedItem.key);
+      newItem.customParams = this.selectedItem.customParams;
       newItem.anchor.setTo(.5);
 
       let eatingAnimation = this.pet.animations.add('eat', [1, 2, 3, 2, 1], 7, false);
       eatingAnimation.onComplete.add(function() {
+        self.updateStats();
         newItem.destroy();
         self.uiBlocked = false;
       }, this);
@@ -146,16 +174,27 @@ var GameState = {
       petMovement.to({x: x, y, y}, 700);
 
       petMovement.onComplete.add(function() {
+        // calculate new stats for pet;
+        for (let key in newItem.customParams) {
+          let result = self.pet.customParams[key] + newItem.customParams[key];
+
+          if (result >= 100) {
+            self.pet.customParams[key] = 100;
+          } else if (result <= 0) {
+            self.pet.customParams[key] = 0;
+          } else {
+            self.pet.customParams[key] = result;
+          }
+
+        }
         eatingAnimation.play('eat');
       });
-
-
 
       petMovement.start();
     }
   },
   clearSelection: function() {
-    this.buttons.forEach(function(button) {
+    this.items.forEach(function(button) {
       button.alpha = 1;
     });
     this.selectedItem = null;
@@ -164,6 +203,31 @@ var GameState = {
   revertSize: function(sprite, event) {
     sprite.scale.setTo(1);
   },
+
+  updateStats: function() {
+    const self = this;
+    for (let key in this.pet.customParams) {
+      // console.log(self[key + 'Text'].style);
+      // if (this.pet.customParams[key] < 20) {
+      //   self[key + 'Text'].stylelowTextStyle.font;
+      // } else {
+      //   self[key = 'Text'].style.cssFont = normalTextStyle.font;
+      // }
+
+      this[key + 'Text'].text = this.pet.customParams[key];
+    }
+  },
+  decreaseStats() {
+    this.pet.customParams.health -= 10;
+    this.pet.customParams.fun -= 15;
+    for (let key in this.pet.customParams) {
+      if (this.pet.customParams[key] <= 0) {
+        this.pet.customParams[key] = 0;
+      }
+    }
+    this.updateStats();
+
+  }
 };
 
 let game = new Phaser.Game(360, 640, Phaser.AUTO); // init new Game; pahser will automatically append a canvas
